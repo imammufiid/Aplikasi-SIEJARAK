a98ce4e98becc745a3e00cd257d45f2b42315cd1
imufiid <imufiid98@gmail.com>
2020-02-04 19:30:30 +0700

    mufiid_landing

--- application/modules/landing/controllers/Landing.php
+++ application/modules/landing/controllers/Landing.php
@@ -19,21 +19,70 @@ class Landing extends CI_Controller
 
 	public function get_jarak()
 	{
-		
-		$latitude1 = -8.118667;
-		$longitude1 = 112.202635;
-		// $latitude1 = $this->input->post('latitude');
-		// $longitude1 = $this->input->post('longitude');
+		$asal = $this->input->post('asal');
+		$tujuan = $this->input->post('tujuan');
 
-		$latitude2 = -7.943795;
-		$longitude2 = 112.659256;
+		$this->db->select('id_kota_asal, id_kota_tujuan, jarak');
+		$this->db->from('graph');
+		$graph = $this->db->get()->result_array();
 
-		$dataJson = file_get_contents("https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=" . $latitude1 . "," . $longitude1 . "&destinations=" . $latitude2 . "," . $longitude2 . "&key=AIzaSyDKCqDcfUH-_Mk1hglPTOjkleBImMFmxPk");
+		// $path = $this->_dijkstra($graph, $asal, $tujuan);
+		var_dump($graph);
 
-		$data = json_decode($dataJson, true);
-		// $nilaiJarak = $data['rows'][0]['elements'][0]['distance']['text'];
+	}
 
-		echo $dataJson;
+	private function _dijkstra($graph_array, $source, $target)
+	{
+		$vertices = array();
+		$neighbours = array();
+		foreach ($graph_array as $edge) {
+			array_push($vertices, $edge[0], $edge[1]);
+			$neighbours[$edge[0]][] = array("end" => $edge[1], "cost" => $edge[2]);
+			$neighbours[$edge[1]][] = array("end" => $edge[0], "cost" => $edge[2]);
+		}
+		$vertices = array_unique($vertices);
+
+		foreach ($vertices as $vertex) {
+			$dist[$vertex] = INF;
+			$previous[$vertex] = NULL;
+		}
+
+		$dist[$source] = 0;
+		$Q = $vertices;
+		while (count($Q) > 0) {
+
+			// TODO - Find faster way to get minimum
+			$min = INF;
+			foreach ($Q as $vertex) {
+				if ($dist[$vertex] < $min) {
+					$min = $dist[$vertex];
+					$u = $vertex;
+				}
+			}
+
+			$Q = array_diff($Q, array($u));
+			if ($dist[$u] == INF or $u == $target) {
+				break;
+			}
+
+			if (isset($neighbours[$u])) {
+				foreach ($neighbours[$u] as $arr) {
+					$alt = $dist[$u] + $arr["cost"];
+					if ($alt < $dist[$arr["end"]]) {
+						$dist[$arr["end"]] = $alt;
+						$previous[$arr["end"]] = $u;
+					}
+				}
+			}
+		}
+		$path = array();
+		$u = $target;
+		while (isset($previous[$u])) {
+			array_unshift($path, $u);
+			$u = $previous[$u];
+		}
+		array_unshift($path, $u);
+		return $path;
 	}
 }
 
